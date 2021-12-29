c01e10652bb3f724f03340765929a66fb1782bcf
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2021-12-29 17:11:16 +0100

    fix: edit profile opens repo in VS Code

--- 1-edit-profile.zsh
+++ 1-edit-profile.zsh
@@ -3,7 +3,7 @@ export PROFILE="$HOME/.zshrc"
 
 # Edit profile
 function config {
-  code --wait ~/.zshrc
-  source ~/.zshrc
+  code --wait ~/.config/zsh
+  source $PROFILE
 }
 alias reload="source $PROFILE"
