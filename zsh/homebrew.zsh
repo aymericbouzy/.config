3c2cfac45df3146987b99aefe8f4ae56e0c931ce
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-01-18 16:58:14 +0100

    feat: homebrew shell completion

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -1,2 +1,10 @@
-# homebrew
 export PATH="/usr/local/sbin:$PATH"
+
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+if type brew &>/dev/null
+then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+  autoload -Uz compinit
+  compinit
+fi
