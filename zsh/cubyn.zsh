eff4d3813c8ab9625902c20c468a882ec25c60f5
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-02-02 13:32:00 +0100

    fix: tw console output

--- zsh/cubyn.zsh
+++ zsh/cubyn.zsh
@@ -1,8 +1,8 @@
 function run {
   local COMMAND="$1"
-  if cat package.json | grep '"dotenv":'; then
+  if cat package.json | grep '"dotenv":' >>/dev/null; then
     dotenv -e .env${ENV:+.$ENV} -- bash -c "$COMMAND"
-  elif cat package.json | grep '"dotenv-flow":'; then
+  elif cat package.json | grep '"dotenv-flow":' >>/dev/null; then
     NODE_ENV=${NODE_ENV:-$ENV} dotenv-flow -- bash -c "$COMMAND"
   else
     echo "You must be using dotenv or dotenv flow in the repo" >>/dev/stderr
@@ -17,6 +17,7 @@ function sql {
 }
 
 function testdb {
+  echo "Recreating test database"
   ENV=test sql 'DROP DATABASE IF EXISTS \`${DB_DATABASE:-$MYSQL_DATABASE}\`; CREATE DATABASE \`${DB_DATABASE:-$MYSQL_DATABASE}\`;'
 }
 
@@ -30,7 +31,15 @@ function testdb-flow {
   NODE_ENV=test dotenv-flow -- bash -c $COMMAND
 }
 
-alias tw="testdb && (make init-test || make test-init) && yarn test:watch"
+function tw {
+  testdb
+  if make help | grep test-init >>/dev/null; then
+    make test-init
+  else
+    make init-test
+  fi
+  yarn test:watch
+}
 
 # available: rabbitmq sftp mongodb elasticsearch product-elasticsearch mysql
 alias devenv="cd $HOME/Dev/cubyn/infra-docker-compose; docker-compose -f datasources.yml up"
