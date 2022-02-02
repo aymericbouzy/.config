f57b3c0a7f00f524bc65a5a19f5a6549eb855e55
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-02-02 11:52:11 +0100

    fix: git global cherry pick
    This will create .rej files instead of doing nothing

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -19,7 +19,7 @@ alias gs="git-sync"
 
 # usage: git-global-cherry-pick service-parcel 5ab7a68
 function git-global-cherry-pick {
-  git --git-dir=../$1/.git format-patch -k -1 --stdout $2 | git am -3 -k
+  git --git-dir=../$1/.git format-patch -k -1 --stdout $2 | git apply --reject --whitespace=fix
 }
 
 # usage: pr-cherry-pick 24
