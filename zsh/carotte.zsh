266429754ad0b1d4f2e6b4212f5e7e7198efd3bf
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-02-08 11:21:06 +0100

    refactor!: krot command

--- zsh/carotte.zsh
+++ zsh/carotte.zsh
@@ -32,36 +32,55 @@ _yargs_completions()
 complete -F _yargs_completions carotte
 ###-end-carotte-completions-###
 
-function migration {
-  filename=`date +%s`-$1.sql;
-  migrationPath='migrations'
+function krot {
+  # usage: krot migration add-deliveries
+  function migration {
+    filename=`date +%s`-$1.sql;
+    migrationPath='migrations'
 
-  if [ -d "migrations/prod" ]
-  then
-    migrationPath='migrations/prod'
-  fi
+    if [ -d "migrations/prod" ]
+    then
+      migrationPath='migrations/prod'
+    fi
 
-  touch $migrationPath/$filename;
-  echo "-- Migration: $1\n-- Created at: `date '+%F %H:%M:%S'`\n\n-- ====  UP  ====\n\n-- ==== DOWN ====" > $migrationPath/$filename;
+    touch $migrationPath/$filename;
+    echo "-- Migration: $1\n-- Created at: `date '+%F %H:%M:%S'`\n\n-- ====  UP  ====\n\n-- ==== DOWN ====" > $migrationPath/$filename;
 
-  if [ -d "migrations/test" ]
-  then
-    cd migrations/test;
-    ln -s ../prod/$filename;
-    cd ../..
-  fi
-  echo "Successfully created $1 migration : ./$migrationPath/$filename"
-}
+    if [ -d "migrations/test" ]
+    then
+      cd migrations/test;
+      ln -s ../prod/$filename;
+      cd ../..
+    fi
+    echo "Successfully created $1 migration : ./$migrationPath/$filename"
+  }
 
-# usage: carotte-create lambda parcel.read:v1
-function carotte-create {
-  local FOLDER="$1"s
-  local QUALIFIER="$2"
-  local QUALIFIER_PATH="./src/$FOLDER/$QUALIFIER"
-  mkdir $QUALIFIER_PATH
-  if [ -f "./src/index.js" ]; then
-    touch $QUALIFIER_PATH/{index,index.spec}.js
-  else
-    touch $QUALIFIER_PATH/{handler,handler.spec,meta}.ts
-  fi
+  function create {
+    local FOLDER="$1"s
+    local QUALIFIER="$2"
+    local QUALIFIER_PATH="./src/$FOLDER/$QUALIFIER"
+    mkdir $QUALIFIER_PATH
+    if [ -f "./src/index.js" ]; then
+      touch $QUALIFIER_PATH/{index,index.spec}.js
+    else
+      touch $QUALIFIER_PATH/{handler,handler.spec,meta}.ts
+    fi
+  }
+
+  # usage: krot controller parcels.create:v1
+  function controller {
+    create controller "$1"
+  }
+
+  # usage: krot lambda parcel.create:v1
+  function lambda {
+    create lambda "$1"
+  }
+
+  # usage: krot listener parcel.created:v1
+  function listener {
+    create listener "$1"
+  }
+
+  "$@"
 }
