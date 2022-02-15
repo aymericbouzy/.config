910787988e278dd29214f47a22a64f376642e623
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-02-15 11:30:07 +0100

    feat: ms

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -18,3 +18,8 @@ function clipboard {
 function csv-to-sql {
   echo "$1" | awk 1 ORS=',' | sed -e 's/.$//' -e 's/^/("/' -e 's/$/")\n/' -e 's/,/","/g' | clipboard
 }
+
+# Displays the number of ms since Unix Epoch
+function ms {
+  node -e 'console.log(Date.now())'
+}
