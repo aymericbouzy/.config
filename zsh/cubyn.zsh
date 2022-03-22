ab1765531ce602c9d0a0d268de6dfc53f130b3b2
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-03-22 09:25:32 +0100

    (wip) feat: cubyn postgres

--- zsh/cubyn.zsh
+++ zsh/cubyn.zsh
@@ -14,9 +14,17 @@ function run {
 }
 
 function sql {
-  local SCRIPT="$1"
-  local USE_DATABASE='CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE:-$MYSQL_DATABASE}\`; USE \`${DB_DATABASE:-$MYSQL_DATABASE}\`'
-  run 'MYSQL_PWD=${DB_PASS:-$MYSQL_PASSWORD} mysql -h ${DB_HOST:-$MYSQL_HOST} --port ${DB_PORT:-${MYSQL_PORT:-3306}} -u ${DB_USER:-$MYSQL_USER} -e "'"$USE_DATABASE; $SCRIPT"'"'
+  if has-dep pg; then
+    if run $'PGPASSWORD=$POSTGRESQL_PASSWORD psql -U $POSTGRESQL_USER -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -tc "SELECT 1 FROM pg_database WHERE datname = \'$POSTGRESQL_DATABASE\'"' | grep -q 1; then
+    else
+      run $'PGPASSWORD=$POSTGRESQL_PASSWORD psql -U $POSTGRESQL_USER -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -c "CREATE DATABASE \'$POSTGRESQL_DATABASE\'"' >> /dev/stderr
+    fi
+    run 'PGPASSWORD=$POSTGRESQL_PASSWORD psql -U $POSTGRESQL_USER -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -c "'$SCRIPT'"'
+  else
+    local SCRIPT="$1"
+    local USE_DATABASE='CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE:-$MYSQL_DATABASE}\`; USE \`${DB_DATABASE:-$MYSQL_DATABASE}\`'
+    run 'MYSQL_PWD=${DB_PASS:-$MYSQL_PASSWORD} mysql -h ${DB_HOST:-$MYSQL_HOST} --port ${DB_PORT:-${MYSQL_PORT:-3306}} -u ${DB_USER:-$MYSQL_USER} -e "'"$USE_DATABASE; $SCRIPT"'"'
+  fi
 }
 
 function testdb {
@@ -44,13 +52,20 @@ alias devenv="cd $HOME/Dev/cubyn/infra-docker-compose; docker-compose -f datasou
 # helm
 export CUBYN_HELM_PATH="$HOME/Dev/cubyn/helm"
 
+# ./shmig -t postgresql -l $$POSTGRESQL_USER -p $$POSTGRESQL_PASSWORD -d $$POSTGRESQL_DATABASE -H $${POSTGRESQL_HOST:-localhost} -P $${POSTGRESQL_PORT:-5432} -s migrations up
+
 function shmig {
+  if has-dep pg; then
+    local engine=postgresql
+  else
+    local engine=mysql
+  fi
   if [ -d migrations/prod ]; then
     local MIGRATIONS_DIRECTORY="migrations/prod"
   else
     local MIGRATIONS_DIRECTORY="migrations"
   fi
-  run './shmig -t mysql -l ${DB_USER:-$MYSQL_USER} -p ${DB_PASS:-$MYSQL_PASSWORD} -d ${DB_DATABASE:-$MYSQL_DATABASE} -H ${DB_HOST:-${MYSQL_HOST:-localhost}} -P ${DB_PORT:-${MYSQL_PORT:-3306}} -m '"$MIGRATIONS_DIRECTORY"' -s migrations '"$@"
+  run './shmig -t '$engine' -l ${DB_USER:-${MYSQL_USER:-$POSTGRESQL_USER}} -p ${DB_PASS:-${MYSQL_PASSWORD:-$POSTGRESQL_PASSWORD}} -d ${DB_DATABASE:-${MYSQL_DATABASE:-$POSTGRESQL_DATABASE}} -H ${DB_HOST:-${MYSQL_HOST:-${POSTGRESQL_HOST:-localhost}}} -P ${DB_PORT:-${MYSQL_PORT:-${POSTGRESQL_PORT:-3306}}} -m '"$MIGRATIONS_DIRECTORY"' -s migrations '"$@"
 }
 
 # usage: s3 /kyivl54o000a30ebe94xncgd.xlsx
