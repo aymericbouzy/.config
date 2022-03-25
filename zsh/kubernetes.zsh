4c904cf1a03a0d89fa89452e9c4566991684f0e9
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-03-25 13:57:06 +0100

    feat: use kubectx + kubens

--- zsh/kubernetes.zsh
+++ zsh/kubernetes.zsh
@@ -1,34 +1,39 @@
-# kubernetes
+
 function kube-staging {
-  gcloud container clusters get-credentials staging --zone europe-west1-b --project staging2-284609
-  export KUBE_NAMESPACE=staging
+  kubectx staging
+  kubens staging
 }
 alias ks="kube-staging"
 
 function kube-qa {
-  gcloud container clusters get-credentials qa --region=europe-west1-b --project=quality-and-assurance
-  export KUBE_NAMESPACE=qa
+  kubectx qa
+  kubens qa
 }
 alias kq="kube-qa"
 
 function kube-demo {
-  gcloud container clusters get-credentials non-critical --zone europe-west3-c --project infra-195110
-  export KUBE_NAMESPACE=sandbox
+  kubectx sandbox
+  kubens sandbox
 }
 alias kd="kube-demo"
 
 function kube-production {
-  gcloud container clusters get-credentials production --region europe-west3 --project infra-195110
-  export KUBE_NAMESPACE=production
+  kubectx production
+  kubens production
 }
 alias kp="kube-production"
 
+function kube-current-context {
+  kubectl config view -o jsonpath='{.current-context}'
+}
+
 function get-pod {
   SERVICE="$1"
-  kubectl -n "$KUBE_NAMESPACE" get pods | grep "$SERVICE" | head -1 | sed -E "s/^($SERVICE-[0-9a-z]{9,10}-[0-9a-z]{5}).*$/\1/"
+  kubectl get pods | grep "$SERVICE" | head -1 | sed -E "s/^($SERVICE-[0-9a-z]{9,10}-[0-9a-z]{5}).*$/\1/"
 }
+
 function pod {
   POD=$(get-pod $1)
-  echo "Connecting to: $POD ($KUBE_NAMESPACE)"
-  kubectl -n "$KUBE_NAMESPACE" exec -ti "$POD" -- bash
+  echo "Connecting to: $POD ($(kube-current-context))"
+  kubectl exec -ti "$POD" -- bash
 }
