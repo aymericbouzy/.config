dc9504810d747d1f47e94f05a9fd1f2870c81442
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-04-08 18:06:39 +0200

    feat: krot start on other env than staging

--- zsh/carotte.zsh
+++ zsh/carotte.zsh
@@ -87,10 +87,10 @@ function krot {
   function start {
     ensure-node-version
     if jq -e '.scripts | has("serve:watch")' package.json >>/dev/null; then
-      NODE_ENV=staging make init
-      NODE_ENV=staging yarn serve:watch
+      NODE_ENV=${NODE_ENV:-staging} make init
+      NODE_ENV=${NODE_ENV:-staging} yarn serve:watch
     else
-      NODE_ENV=staging make run-watch
+      NODE_ENV=${NODE_ENV:-staging} make run-watch
     fi
   }
 
