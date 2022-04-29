60cb82931f0f1fe6f6ff065a1178973d800f292d
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-04-29 22:50:34 +0200

    feat(git): switch fzf

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -76,3 +76,36 @@ function pending-release {
 function git-post-checkout {
   ensure-node-version
 }
+
+# from https://polothy.github.io/post/2019-08-19-fzf-git-checkout/
+function fzf-git-branch {
+  git rev-parse HEAD > /dev/null 2>&1 || return
+
+  git branch --color=always --all --sort=-committerdate |
+    grep -v HEAD |
+    fzf --height 50% --ansi --no-multi --preview-window right:65% \
+      --preview 'git log -n 50 --color=always --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed "s/.* //" <<< {})' |
+    sed "s/.* //"
+}
+
+function switch {
+  git rev-parse HEAD > /dev/null 2>&1 || return
+
+  local branch
+
+  branch=$(fzf-git-branch)
+  if [[ "$branch" = "" ]]; then
+    return
+  fi
+
+  # If branch name starts with 'remotes/' then it is a remote branch. By
+  # using --track and a remote branch name, it is the same as:
+  # git checkout -b branchName --track origin/branchName
+  if [[ "$branch" = 'remotes/'* ]]; then
+    git sw --track $branch
+  else
+    git sw $branch
+  fi
+}
+
+alias sw=switch
