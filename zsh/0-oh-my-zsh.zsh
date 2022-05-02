182f9c30e7f7de0242efd48fa0284359c26afff1
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-05-02 11:30:59 +0200

    feat(zsh): zsh-completions

--- zsh/0-oh-my-zsh.zsh
+++ zsh/0-oh-my-zsh.zsh
@@ -72,6 +72,9 @@ ZSH_THEME="robbyrussell"
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git command-not-found fzf-tab)
 
+# https://github.com/zsh-users/zsh-completions
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
