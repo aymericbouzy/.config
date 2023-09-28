64c546c35905ee5eafc9341caf9e10a4870a9b68
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2023-09-28 14:47:06 +0200

    fix: keep CAROTTE_HOST in sync

--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -1,40 +1,27 @@
-function set-carotte-host {
-  local env="$(kubens -c)"
-
-  case $env in
-  staging)
-    export CAROTTE_HOST=rabbitmq-amqp.staging.playship.co
-  ;;
-  production)
-    export CAROTTE_HOST=rabbitmq-amqp.production.playship.co
-  ;;
-  sandbox)
-    export CAROTTE_HOST=
-  ;;
-  *)
-    export CAROTTE_HOST=
-  ;;
-  esac
-}
-
-
 function staging {
   kubectx staging
   kubens staging
-  set-carotte-host
 }
 
 function production {
   kubectx production
   kubens production
-  set-carotte-host
 }
 alias prod=production
 
 function sandbox {
   kubectx production
   kubens sandbox
-  set-carotte-host
 }
 
-set-carotte-host
+function precmd {
+  local env="$(kubectl config view -o jsonpath='{.current-context}')"
+
+  if [[ $env == staging ]]; then
+    export CAROTTE_HOST=rabbitmq-amqp.staging.playship.co
+  elif [[ $env == production ]]; then
+    export CAROTTE_HOST=rabbitmq-amqp.production.playship.co
+  else
+    export CAROTTE_HOST=
+  fi
+}
