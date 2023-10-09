cfaf3407756ccdd88fea7566bf46667cef34a356
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2023-10-09 14:53:33 +0200

    feat(envs): herblay(-p2)?

--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -16,12 +16,37 @@ function sandbox {
 
 function precmd {
   local env="$(kubectl config view -o jsonpath='{.current-context}')"
+  local vhost="$(kubens -c)"
+  local user="$vhost"
+
+  if [[ $env == $vhost ]]; then
+    local vhost=""
+    local user="guest"
+  fi
+
+  local password="$(rabbitmq_password $env $user)"
 
   if [[ $env == staging ]]; then
-    export CAROTTE_HOST=rabbitmq-amqp.staging.playship.co
+    export CAROTTE_HOST="amqp://$user:$password@rabbitmq-amqp.staging.playship.co/$vhost"
   elif [[ $env == production ]]; then
-    export CAROTTE_HOST=rabbitmq-amqp.production.playship.co
+    export CAROTTE_HOST="amqp://$user:$password@rabbitmq-amqp.production.playship.co/$vhost"
   else
     export CAROTTE_HOST=
+  fi
+}
+
+function rabbitmq_password {
+  if [[ $1 == production ]]; then
+    if [[ $2 == guest ]]; then
+      echo $CUBYN_RABBITMQ_GUEST_PASSWORD
+    elif [[ $2 == herblay ]]; then
+      echo $CUBYN_RABBITMQ_HERBLAY_PASSWORD
+    elif [[ $2 == herblay-p2 ]]; then
+      echo $CUBYN_RABBITMQ_HERBLAY_P2_PASSWORD
+    fi
+  elif [[ $1 == staging ]]; then
+    if [[ $2 == guest ]]; then
+      echo $CUBYN_RABBITMQ_GUEST_PASSWORD
+    fi
   fi
 }
